1ee8a358ab747f26f90faa2152abb1975f109a17
gilber_carlog <alu2752@gmail.com>
2015-06-05 04:11:21 +0100

    - La lista de logs pasa a ser ObservableListView material design con imagen

--- app/src/main/java/com/carlog/gilberto/carlog/DatosIniciales.java
+++ app/src/main/java/com/carlog/gilberto/carlog/DatosIniciales.java
@@ -1,39 +1,71 @@
 package com.carlog.gilberto.carlog;
 
+import android.annotation.TargetApi;
 import android.app.Activity;
 import android.app.AlertDialog;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.res.Configuration;
+import android.content.res.TypedArray;
 import android.database.Cursor;
+import android.os.Build;
 import android.os.Bundle;
+import android.util.TypedValue;
 import android.view.ContextMenu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.AbsListView;
 import android.widget.AdapterView;
-import android.widget.Button;
-import android.widget.ListView;
+import android.widget.FrameLayout;
 import android.widget.TextView;
 
 import com.carlog.gilberto.carlog.data.DBCar;
 import com.carlog.gilberto.carlog.data.DBLogs;
 import com.gc.materialdesign.views.ButtonFloat;
+import com.github.ksoichiro.android.observablescrollview.ObservableListView;
+import com.github.ksoichiro.android.observablescrollview.ObservableScrollViewCallbacks;
+import com.github.ksoichiro.android.observablescrollview.ScrollState;
+import com.github.ksoichiro.android.observablescrollview.ScrollUtils;
+import com.nineoldandroids.view.ViewHelper;
+import com.nineoldandroids.view.ViewPropertyAnimator;
+
+import android.support.v7.app.AppCompatActivity;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
 
+
+
 /**
  * Created by Gilberto on 29/10/2014.
  */
-public class DatosIniciales extends Activity {
+public class DatosIniciales extends AppCompatActivity implements ObservableScrollViewCallbacks  {
 
-    private ArrayList<String> datos_lista_log = new ArrayList<String>();
+    private static final float MAX_TEXT_SCALE_DELTA = 0.3f;
+
+    private ObservableListView listView;
+    private View mImageView;
+    private View mOverlayView;
+    private View mListBackgroundView;
+    private TextView mTitleView;
+    private View mFab;
+    private int mActionBarSize;
+    private int mFlexibleSpaceShowFabOffset;
+    private int mFlexibleSpaceImageHeight;
+    private int mFabMargin;
+    private boolean mFabIsShown;
 
 
-    private void borrarLogpulsado(ListView lv, final Cursor cursor, final DBLogs manager, final int posicion) {
+
+
+
+
+    private void borrarLogpulsado(final Cursor cursor, final DBLogs manager, final int posicion) {
 
 
                 AlertDialog.Builder builder = new AlertDialog.Builder(DatosIniciales.this);
@@ -53,7 +85,7 @@ public class DatosIniciales extends Activity {
                                         //Recorremos el cursor
                                         int i = 0;
                                         for(cursor.moveToFirst(); !cursor.isAfterLast(); cursor.moveToNext()){
-                                            if (i == posicion) { // la posicion del cursor coincide con la del que pulsamos en la lista
+                                            if (i == posicion-1) { // la posicion del cursor coincide con la del que pulsamos en la lista
                                                 int id = cursor.getInt(cursor.getColumnIndex(DBLogs.CN_ID));
                                                 String matricula = cursor.getString(cursor.getColumnIndex(DBLogs.CN_CAR));
                                                 manager.eliminar_por_id(id);
@@ -72,7 +104,7 @@ public class DatosIniciales extends Activity {
     }
 
 
-    private void modificarLogpulsado(ListView lv, final Cursor cursor, final DBLogs manager, int posicion) {
+    private void modificarLogpulsado(final Cursor cursor, final DBLogs manager, int posicion) {
         final TipoCoche miCoche = (TipoCoche)getIntent().getExtras().getSerializable("miCoche");
 
         //Recorremos el cursor
@@ -80,7 +112,7 @@ public class DatosIniciales extends Activity {
         String tipo = "";
         Intent intent = new Intent();
         for(cursor.moveToFirst(); !cursor.isAfterLast(); cursor.moveToNext()){
-            if (i == posicion) { // la posicion del cursor coincide con la del que pulsamos en la lista
+            if (i == posicion-1) { // la posicion del cursor coincide con la del que pulsamos en la lista
                 int id = cursor.getInt(cursor.getColumnIndex(DBLogs.CN_ID));
                 tipo = cursor.getString(cursor.getColumnIndex(DBLogs.CN_TIPO));
                 if(tipo.equals(TipoLog.TIPO_ACEITE)) {
@@ -114,14 +146,14 @@ public class DatosIniciales extends Activity {
 
     }
 
-    private void realizadoLogpulsado(ListView lv, Cursor cursor, DBLogs manager, int posicion) {
+    private void realizadoLogpulsado(Cursor cursor, DBLogs manager, int posicion) {
         final TipoCoche miCoche = (TipoCoche)getIntent().getExtras().getSerializable("miCoche");
 
         //Recorremos el cursor
         int i = 0;
         for(cursor.moveToFirst(); !cursor.isAfterLast(); cursor.moveToNext()){
 
-            if (i == posicion) { // la posicion del cursor coincide con la del que pulsamos en la lista
+            if (i == posicion-1) { // la posicion del cursor coincide con la del que pulsamos en la lista
                 int id = cursor.getInt(cursor.getColumnIndex(DBLogs.CN_ID));
                 String matricula = cursor.getString(cursor.getColumnIndex(DBLogs.CN_CAR));
 
@@ -163,12 +195,6 @@ public class DatosIniciales extends Activity {
         cursor.moveToFirst();
         //while (cursor.moveToNext()) {
         for(cursor.moveToFirst(); !cursor.isAfterLast(); cursor.moveToNext()){
-            System.out.println("KKKK "+k);
-            if(cursor.getString(cursor.getColumnIndex(DBLogs.CN_TIPO)).equals(TipoLog.TIPO_ACEITE)) {
-                System.out.println("en consultarlogs date " + funciones.string_a_date(cursor.getString(cursor.getColumnIndex("fecha_string"))));
-                System.out.println("en consultarlogs string " + cursor.getString(cursor.getColumnIndex("fecha_string")));
-                System.out.println("en consultarlogs int " + funciones.string_a_int(cursor.getString(cursor.getColumnIndex("fecha_string"))));
-            }
             TipoLog miTipoLog = new TipoLog(cursor.getString(cursor.getColumnIndex(DBLogs.CN_TIPO)),funciones.string_a_date(cursor.getString(cursor.getColumnIndex("fecha_string"))), cursor.getString(cursor.getColumnIndex("fecha_string")),
                     funciones.string_a_int(cursor.getString(cursor.getColumnIndex("fecha_string"))), cursor.getInt(cursor.getColumnIndex(DBLogs.CN_ACEITE)), cursor.getString(cursor.getColumnIndex(DBLogs.CN_CAR)),
                     cursor.getInt(cursor.getColumnIndex(DBLogs.CN_REALIZADO)), cursor.getInt(cursor.getColumnIndex(DBLogs.CN_KMS)));
@@ -182,29 +208,77 @@ public class DatosIniciales extends Activity {
 
        // final SimpleCursorAdapter adaptador = new SimpleCursorAdapter(this, android.R.layout.two_line_list_item, cursor, from, to);
         //Asociamos el adaptador a la vista.
-        ListView lv = (ListView) findViewById(R.id.lista_log);
 
 
         miAdaptadorLog adapter = new miAdaptadorLog(this, listaLogs);
 
 
-        lv.setAdapter(adapter);
-       // lv.setAdapter(adaptador);
+        listView.setAdapter(adapter);
 
 
 
 
         //Asociamos el menú contextual a los controles
-        registerForContextMenu(lv);
+        registerForContextMenu(listView);
 
 
     }
 
+    protected int getActionBarSize() {
+        TypedValue typedValue = new TypedValue();
+        int[] textSizeAttr = new int[]{R.attr.actionBarSize};
+        int indexOfAttrTextSize = 0;
+        TypedArray a = obtainStyledAttributes(typedValue.data, textSizeAttr);
+        int actionBarSize = a.getDimensionPixelSize(indexOfAttrTextSize, -1);
+        a.recycle();
+        return actionBarSize;
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        // TODO Auto-generated method stub
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_datos_iniciales);
+
+
+
+        mFlexibleSpaceImageHeight = getResources().getDimensionPixelSize(R.dimen.flexible_space_image_height);
+        mFlexibleSpaceShowFabOffset = getResources().getDimensionPixelSize(R.dimen.flexible_space_show_fab_offset);
+        mActionBarSize = getActionBarSize();
+        mImageView = findViewById(R.id.image);
+        mOverlayView = findViewById(R.id.overlay);
+        listView = (ObservableListView) findViewById(R.id.list);
+        listView.setScrollViewCallbacks(this);
+
+        // Set padding view for ListView. This is the flexible space.
+        View paddingView = new View(this);
+        AbsListView.LayoutParams lp = new AbsListView.LayoutParams(AbsListView.LayoutParams.MATCH_PARENT,
+                mFlexibleSpaceImageHeight);
+        paddingView.setLayoutParams(lp);
+
+        // This is required to disable header's list selector effect
+        paddingView.setClickable(true);
+
+        listView.addHeaderView(paddingView);
+        //setDummyData(listView);
+        mTitleView = (TextView) findViewById(R.id.title);
+        mTitleView.setText("Logs próximos");
+        setTitle(null);
+        mFab = findViewById(R.id.fab);
+        mFab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(DatosIniciales.this, "FAB is clicked", Toast.LENGTH_SHORT).show();
+            }
+        });
+        mFabMargin = getResources().getDimensionPixelSize(R.dimen.margin_standard);
+        ViewHelper.setScaleX(mFab, 0);
+        ViewHelper.setScaleY(mFab, 0);
+
+        // mListBackgroundView makes ListView's background except header view.
+        mListBackgroundView = findViewById(R.id.list_background);
+
+
+
 
 
        // final TipoCoche miCoche = (TipoCoche)getIntent().getExtras().getSerializable("miCoche");
@@ -278,6 +352,96 @@ public class DatosIniciales extends Activity {
 
     }
 
+
+    @Override
+    public void onScrollChanged(int scrollY, boolean firstScroll, boolean dragging) {
+        // Translate overlay and image
+        float flexibleRange = mFlexibleSpaceImageHeight - mActionBarSize;
+        int minOverlayTransitionY = mActionBarSize - mOverlayView.getHeight();
+        ViewHelper.setTranslationY(mOverlayView, ScrollUtils.getFloat(-scrollY, minOverlayTransitionY, 0));
+        ViewHelper.setTranslationY(mImageView, ScrollUtils.getFloat(-scrollY / 2, minOverlayTransitionY, 0));
+
+        // Translate list background
+        ViewHelper.setTranslationY(mListBackgroundView, Math.max(0, -scrollY + mFlexibleSpaceImageHeight));
+
+        // Change alpha of overlay
+        ViewHelper.setAlpha(mOverlayView, ScrollUtils.getFloat((float) scrollY / flexibleRange, 0, 1));
+
+        // Scale title text
+        float scale = 1 + ScrollUtils.getFloat((flexibleRange - scrollY) / flexibleRange, 0, MAX_TEXT_SCALE_DELTA);
+        setPivotXToTitle();
+        ViewHelper.setPivotY(mTitleView, 0);
+        ViewHelper.setScaleX(mTitleView, scale);
+        ViewHelper.setScaleY(mTitleView, scale);
+
+        // Translate title text
+        int maxTitleTranslationY = (int) (mFlexibleSpaceImageHeight - mTitleView.getHeight() * scale);
+        int titleTranslationY = maxTitleTranslationY - scrollY;
+        ViewHelper.setTranslationY(mTitleView, titleTranslationY);
+
+        // Translate FAB
+        int maxFabTranslationY = mFlexibleSpaceImageHeight - mFab.getHeight() / 2;
+        float fabTranslationY = ScrollUtils.getFloat(
+                -scrollY + mFlexibleSpaceImageHeight - mFab.getHeight() / 2,
+                mActionBarSize - mFab.getHeight() / 2,
+                maxFabTranslationY);
+        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.HONEYCOMB) {
+            // On pre-honeycomb, ViewHelper.setTranslationX/Y does not set margin,
+            // which causes FAB's OnClickListener not working.
+            FrameLayout.LayoutParams lp = (FrameLayout.LayoutParams) mFab.getLayoutParams();
+            lp.leftMargin = mOverlayView.getWidth() - mFabMargin - mFab.getWidth();
+            lp.topMargin = (int) fabTranslationY;
+            mFab.requestLayout();
+        } else {
+            ViewHelper.setTranslationX(mFab, mOverlayView.getWidth() - mFabMargin - mFab.getWidth());
+            ViewHelper.setTranslationY(mFab, fabTranslationY);
+        }
+
+        // Show/hide FAB
+        if (fabTranslationY < mFlexibleSpaceShowFabOffset) {
+            hideFab();
+        } else {
+            showFab();
+        }
+    }
+
+    @Override
+    public void onDownMotionEvent() {
+    }
+
+    @Override
+    public void onUpOrCancelMotionEvent(ScrollState scrollState) {
+    }
+
+    @TargetApi(Build.VERSION_CODES.JELLY_BEAN_MR1)
+    private void setPivotXToTitle() {
+        Configuration config = getResources().getConfiguration();
+        if (Build.VERSION_CODES.JELLY_BEAN_MR1 <= Build.VERSION.SDK_INT
+                && config.getLayoutDirection() == View.LAYOUT_DIRECTION_RTL) {
+            ViewHelper.setPivotX(mTitleView, findViewById(android.R.id.content).getWidth());
+        } else {
+            ViewHelper.setPivotX(mTitleView, 0);
+        }
+    }
+
+    private void showFab() {
+        if (!mFabIsShown) {
+            ViewPropertyAnimator.animate(mFab).cancel();
+            ViewPropertyAnimator.animate(mFab).scaleX(1).scaleY(1).setDuration(200).start();
+            mFabIsShown = true;
+        }
+    }
+
+    private void hideFab() {
+        if (mFabIsShown) {
+            ViewPropertyAnimator.animate(mFab).cancel();
+            ViewPropertyAnimator.animate(mFab).scaleX(0).scaleY(0).setDuration(200).start();
+            mFabIsShown = false;
+        }
+    }
+
+
+
     @Override
     public void onCreateContextMenu(ContextMenu menu, View v,
                                     ContextMenu.ContextMenuInfo menuInfo)
@@ -298,7 +462,6 @@ public class DatosIniciales extends Activity {
         int int_now = funciones.date_a_int(new Date());
         Context contextNew = getApplicationContext();
         final DBLogs manager = new DBLogs(contextNew);
-        ListView lv = (ListView) findViewById(R.id.lista_log);
 
 
 
@@ -312,13 +475,13 @@ public class DatosIniciales extends Activity {
 
         switch (item.getItemId()) {
             case R.id.menu_modificarLog:
-                modificarLogpulsado(lv, cursor, manager, info.position);
+                modificarLogpulsado(cursor, manager, info.position);
                 return true;
             case R.id.menu_eliminarLog:
-                borrarLogpulsado(lv, cursor, manager, info.position);
+                borrarLogpulsado(cursor, manager, info.position);
                 return true;
             case R.id.menu_realizadoLog:
-                realizadoLogpulsado(lv, cursor, manager, info.position);
+                realizadoLogpulsado(cursor, manager, info.position);
                 return true;
             default:
                 return super.onContextItemSelected(item);
